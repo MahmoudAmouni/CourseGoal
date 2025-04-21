ecb71f3b865de86787f4003f41f3e55e4e64c36d
ZAZALOmBRE <157347568+buman0@users.noreply.github.com>
2025-04-21 13:38:50 +0300

    Added new feature

--- src/Components/CourseGoal.tsx
+++ src/Components/CourseGoal.tsx
@@ -2,14 +2,16 @@ import { ReactNode } from "react"
 
 interface CGProps{
     title:string,
-    children:ReactNode
+    children:ReactNode,
+    id:number,
+    handledltGoal:(id:number)=> void
 }
-export default function CourseGoal({title,children}:CGProps) {
+export default function CourseGoal({title,children,id,handledltGoal}:CGProps) {
   return (
     <article>
       <h1>{title}</h1>
       <h2>{children}</h2>
-      <p>Delete</p>
+      <p onClick={()=>handledltGoal(id)}>Delete</p>
     </article>
   );
 }
