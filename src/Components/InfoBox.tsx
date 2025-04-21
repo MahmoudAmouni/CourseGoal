976b1da467b069cd1e9b0b984c409d50a65a2f2c
ZAZALOmBRE <157347568+buman0@users.noreply.github.com>
2025-04-21 16:24:05 +0300

    Added new feature

---
+++ src/Components/InfoBox.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react"
+
+interface IBProps{
+    children:ReactNode,
+    mode:"hint" | "warning"
+}
+export default function InfoBox({children,mode}:IBProps) {
+    if(mode==="hint"){
+        return (
+          <aside className="infobox infobox-hint">
+            <p>{children}</p>
+          </aside>
+        );
+    }
+    return (
+      <aside className="infobox infobox-hint">
+        <h2>Warning</h2>
+        <p>{children}</p>
+      </aside>
+    );
+  
+}
