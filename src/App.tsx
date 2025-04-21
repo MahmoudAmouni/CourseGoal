23040f58cbe6d62eb5044ab93aa19771e1dec7a2
ZAZALOmBRE <157347568+buman0@users.noreply.github.com>
2025-04-21 13:31:52 +0300

    Added new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
 import CourseGoal from "./Components/CourseGoal";
 import Header from "./Components/Header";
 import imgsrc from "./assets/goals.jpg"
+
+export interface CourseGoalsType {
+  title: string;
+  description: string;
+  id: number;
+}
 export default function App() {
+  
+  const [goals, setGoals] = useState<CourseGoalsType[]>([]);
+
+  function handleAddGoal(){
+    setGoals((goals)=>{
+      const newGoal: CourseGoalsType={
+        title:"new Goal",
+        description:"goal description",
+        id:Math.random()
+      }
+      return [...goals,newGoal]
+    })
+  }
   return (
     <main>
       <Header img={{ src: imgsrc, alt: "img alt" }}>
         <h1>Your Course Goal</h1>
       </Header>
-      <CourseGoal title="the course title">
-        The course goal of this course
-      </CourseGoal>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+             {goal.description}
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
