1d0886ad3f739cb8516f74fd1b06d4188152a065
ZAZALOmBRE <157347568+buman0@users.noreply.github.com>
2025-04-21 16:03:08 +0300

    Added new feature

--- src/Components/NewGoal.tsx
+++ src/Components/NewGoal.tsx
@@ -6,11 +6,12 @@ interface NGProps{
 export default function NewGoal({handleNewGoal}:NGProps) {
     const goal = useRef<HTMLInputElement>(null);
     const summary = useRef<HTMLInputElement>(null);
-    function handleSubmit(e:FormEvent){
+    function handleSubmit(e:FormEvent<HTMLFormElement>){
         e.preventDefault();
         const enteredGoal = goal.current!.value;
         const enteredSummary = summary.current!.value;
 
+        e.currentTarget.reset()
         handleNewGoal(enteredGoal,enteredSummary);
         
     }
@@ -18,14 +19,14 @@ export default function NewGoal({handleNewGoal}:NGProps) {
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your goal</label>
-        <input id="goal" type="text" />
+        <input id="goal" type="text" ref={goal} />
       </p>
 
       <p>
         <label htmlFor="summary">Short summary</label>
-        <input id="summary" type="text" />
+        <input id="summary" type="text" ref={summary} />
       </p>
-      <button>Add Goal</button>
+      <button type="submit">Add Goal</button>
     </form>
   );
 }
